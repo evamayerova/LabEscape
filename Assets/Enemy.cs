378e74f0285303151d9c448ecdd74beaec57bfe4
Eliška Kuběnová <kubenova.e@gmail.com>
2016-12-01 23:00:20 +0100

    create watch range and shoot range on enemy creation

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,31 +4,52 @@ using System.Collections;
 public class Enemy : Character {
 
     const string defaultCharType = "technican";
-    public float watchRange;
 
-    // private CircleCollider2D rangeToWatch;
-    // private BoxCollider2D rangeToShoot;
+	private GameObject hearing, shooting;
+	private CircleCollider2D rangeToWatch;
+    private BoxCollider2D rangeToShoot;
     // Use this for initialization
     void Start () {
         MoveDir = 1.0f;
         CharacterType = defaultCharType;
         setDefaultStats();
-        // rangeToWatch = gameObject.AddComponent<CircleCollider2D>();
-        // rangeToWatch.isTrigger = true;
-        // rangeToWatch.radius = 5.0f;// watchRange;
-        // rangeToShoot = gameObject.AddComponent<BoxCollider2D>();
-        // rangeToShoot.size = new Vector2(2.0f, 1.0f);
-        // rangeToShoot.isTrigger = true;
-    }
-	
+
+		hearing = new GameObject ();
+		shooting = new GameObject ();
+		setRangeParams(hearing, "c", HearingRange, "Watch Range Collider");
+		setRangeParams(shooting, "b", ShootingRange, "Shoot Range Collider");
+	}
+
+	void setRangeParams(GameObject go, string type, float range, string name)
+	{
+		go.transform.parent = gameObject.transform;
+		go.transform.position = gameObject.transform.position;
+		go.name = name;
+		switch(type)
+		{
+		case "c":
+		{
+			// hearing area
+			rangeToWatch = go.AddComponent<CircleCollider2D>();
+			rangeToWatch.radius = HearingRange;
+			rangeToWatch.isTrigger = true;
+			go.AddComponent<WatchRange>();
+		}
+			break;
+		case "b":
+		{
+			// shooting area
+			rangeToShoot = go.AddComponent<BoxCollider2D>();
+			rangeToShoot.size = new Vector2(ShootingRange, 1.0f);
+			rangeToShoot.isTrigger = true;
+			go.AddComponent<ShootRange>();
+		}
+			break;
+		}
+	}
 	// Update is called once per frame
 	void Update ()
     {
         gameObject.GetComponentInChildren<Shooter>().shoot();
 	}
-
-    void OnTriggerEnter2D(Collider2D c)
-    {
-        Debug.Log("Circle collider " + c.name);
-    }
 }
